f76c091a09ed2beff4d430cb79192f95b2b05411
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2022-02-17 12:21:01 +0800

    Create 1626A.cpp

---
+++ 1626A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+	int n; cin>>n;
+	for(int i = 0;i<n;i++){
+		string s; cin>>s;
+		sort(s.begin(),s.end());
+		cout<<s<<"\n";
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	}
+}
+	
