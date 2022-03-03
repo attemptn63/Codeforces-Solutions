4ca39d419599f697db663ddcff73f37e85348fdc
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2022-03-03 11:45:39 +0800

    Create 4A.cpp

---
+++ 4A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define SPEED ios_base::sync_with_stdio(0);cin.tie(NULL);
+int main() {
+	SPEED
+	ll n;cin>>n;
+	if(n == 2){cout<<"NO";return 0;}
+	if(n%2==0)cout<<"YES";
+	else cout<<"NO";
+}
