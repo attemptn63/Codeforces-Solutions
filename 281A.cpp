1f0192d2199233736814e559d8d0788a501d97cc
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2022-03-08 13:13:15 +0800

    Create 281A.cpp

---
+++ 281A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define speed ios_base::sync_with_stdio(0);cin.tie(NULL);
+typedef pair<int,int>pi;
+int32_t main(){
+    string s;cin>>s;
+    s[0] = toupper(s[0]);
+    cout<<s;
+}
