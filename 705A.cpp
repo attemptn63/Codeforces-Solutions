1222e80cd4bbf62ef3fb4c3ee6014f5da06b33df
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2022-04-08 08:12:28 +0800

    Create 705A.cpp

---
+++ 705A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pi pair<int,int>
+#define s second
+#define f first
+main(){
+    int n;cin>>n;
+    cout<<"I hate";
+    for(int i = 1;i<n;i++){
+        cout<<" that";
+        if(i%2==1)cout<<" I love";
+        else cout<<" I hate";
+    }
+    cout<<" it";
+}
