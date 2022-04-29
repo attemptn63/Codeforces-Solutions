f4a728a4b82f89e50ad1277e20379e2bc126c598
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2022-04-29 14:30:45 +0800

    Create 1426B.cpp

---
+++ 1426B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define speed ios_base::sync_with_stdio(false);cin.tie(NULL); 
+#define pi pair<int,int> 
+#define pii pair<int,pi>
+#define f first
+#define s second
+int arr[101];
+main(){
+	speed
+	int t;cin>>t;
+	for(int i = 0;i<t;i++){
+		int n;cin>>n;
+		for(int i = 0;i<n;i++){
+			cin>>arr[i];
+		}
+		sort(arr,arr+n,greater<int>());
+		for(int i = 0;i<n;i++){
+			cout<<arr[i]<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+
+
