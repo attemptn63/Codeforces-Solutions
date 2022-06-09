32fbe2dab468e0d474df66dcd75fda9b70f8e7c6
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2022-06-09 09:55:19 +0800

    Create 337A.cpp

---
+++ 337A.cpp
@@ -0,0 +1,28 @@
+
+using namespace std;
+#define ll long long
+#define speed ios_base::sync_with_stdio(0);cin.tie(NULL);
+#define pi pair<int,int>
+#define pb(x) push_back(x)
+#define pob() pop_back()
+
+int main() {
+	speed
+	int n, m, f[1000];
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> f[i];
+    }
+    sort(f, f + m);
+    int least = f[n-1] - f[0];
+    for (int i = 1; i <= m - n; ++i)
+    {
+        if (f[i+n-1] - f[i] < least)
+        {
+            least = f[i+n-1] - f[i];
+        }
+    }
+    cout << least << endl;
+    return 0;
+}
